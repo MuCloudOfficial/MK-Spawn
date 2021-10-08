29522c9b86d34c107a6a6f726b89c37d1824fcaa
Mark_Chanel <Mark_Chanel@outlook.fr>
2021-10-08 12:58:49 +0800

    建立基本结构.

--- src/main/java/me/markchanel/plugin/EX/AdvancedWarps/Listeners.java
+++ src/main/java/me/markchanel/plugin/EX/AdvancedWarps/Listeners.java
@@ -8,6 +8,7 @@ import org.bukkit.Location;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -20,19 +21,30 @@ public class Listeners implements Listener {
 
     @EventHandler
     private void WarpCommandListener(UserWarpEvent uwe) throws NullPointerException {
+        uwe.setCancelled(true);
         String target = uwe.getWarp();
-        IUser targetP = uwe.getUser();
+        IUser targetU = uwe.getUser();
+        Player targetP = targetU.getBase();
+        Location targetLoc = Config.Warps.get(target);
         if (Config.Warps.containsKey(target)) {
             if(Config.RequiredPermissionWarps.containsKey(target)){
-                if(!targetP.getBase().hasPermission(Config.RequiredPermissionWarps.get(target))){
-                    targetP.sendMessage(Main.Prefix + ChatColor.RED + "你没有权限传送至目标地");
-                    uwe.setCancelled(true);
+                if(!targetP.hasPermission(Config.RequiredPermissionWarps.get(target))){
+                    targetU.sendMessage(Main.Prefix + ChatColor.RED + "你没有权限传送至目标地");
+                    return;
                 }
-            } else if (Config.RequiredItemWarps.containsKey(target)) {
+                teleportTo(targetP,targetLoc);
+            }
+
+            if(!targetP.hasPermission("essentials.warps."+target)){
+                targetU.sendMessage(Main.Prefix + ChatColor.RED + "你没有权限传送至目标地");
+                return;
+            }
+
+            if (Config.RequiredItemWarps.containsKey(target)) {
                 List<Object> itemInfo = Config.RequiredItemWarps.get(target);
                 boolean qualified = false;
                 int amount = 0;
-                for(ItemStack is : targetP.getBase().getInventory().getContents()){
+                for(ItemStack is : targetP.getInventory().getContents()){
                     if(is.getType().name().equals(itemInfo.get(0)) &&
                             is.getItemMeta().getDisplayName().equals(itemInfo.get(1)) &&
                             is.getItemMeta().getLore().equals(itemInfo.get(2))){
@@ -44,14 +56,20 @@ public class Listeners implements Listener {
                 }
                 if(!qualified){
                     targetP.sendMessage(Main.Prefix + ChatColor.RED + "你未持有必需的物品或当前持有的必需物品数不足以允许你传送至目标地");
-                    uwe.setCancelled(true);
-                }
-            } else if (Config.RequiredMoneyWarps.containsKey(target)) {
-                if(targetP.getMoney().doubleValue() < Config.RequiredMoneyWarps.get(target)){
-                    targetP.sendMessage(Main.Prefix + ChatColor.RED + "你没有足够的金钱传送至目标地");
-                    uwe.setCancelled(true);
+                    return;
+                }else{
+                    teleportTo(targetP,targetLoc);
                 }
             }
+
+            if (Config.RequiredMoneyWarps.containsKey(target)) {
+                if (targetU.getMoney().doubleValue() < Config.RequiredMoneyWarps.get(target)) {
+                    targetP.sendMessage(Main.Prefix + ChatColor.RED + "你没有足够的金钱传送至目标地");
+                    return;
+                }
+                teleportTo(targetP,targetLoc);
+            }
+            teleportTo(targetP,targetLoc);
         }
     }
 
@@ -60,11 +78,13 @@ public class Listeners implements Listener {
         FileConfiguration f = new YamlConfiguration();
         wme.setCancelled(true);
         IUser target = wme.getUser();
+        Player targetP = target.getBase();
         Location targetLoc = wme.getNewLocation();
+        String targetN = wme.getWarpName();
         switch (wme.getCause()){
             case CREATE:
                 Config.Warps.put(wme.getWarpName(),wme.getNewLocation());
-                File targetCreateFile = new File(Config.EssC.getConfigFile().getParentFile().getAbsolutePath() + File.separator + "warps" + File.separator + wme.getWarpName() + ".yml");
+                File targetCreateFile = new File(Config.EssC.getConfigFile().getParentFile().getAbsolutePath() + File.separator + "warps" + File.separator + targetN + ".yml");
                 try {
                     f.load(targetCreateFile);
                     f.set("world",targetLoc.getWorld().getUID());
@@ -74,18 +94,20 @@ public class Listeners implements Listener {
                     f.set("z",targetLoc.getZ());
                     f.set("yaw",targetLoc.getYaw());
                     f.set("pitch",targetLoc.getPitch());
-                    f.set("name",wme.getWarpName());
-                    f.set("lastowner",wme.getUser().getBase().getUniqueId());
+                    f.set("name",targetN);
+                    f.set("lastowner",targetP.getUniqueId());
                     f.save(targetCreateFile);
                 } catch (IOException | InvalidConfigurationException e) {
                     e.printStackTrace();
                 }
-                target.sendMessage(Main.Prefix + ChatColor.GREEN + "你创建了一个新地标 " + wme.getWarpName() + " 位置: " + wme.getNewLocation());
+                Config.refreshWarpFiles();
+                target.sendMessage(Main.Prefix + ChatColor.GREEN + "你创建了一个新地标 " + targetN);
                 break;
             case DELETE:
-                Config.Warps.remove(wme.getWarpName());
-                Config.getWarpFile(wme.getWarpName()).delete();
-                target.sendMessage(Main.Prefix + ChatColor.YELLOW + "你删除了一个地标 " + wme.getWarpName());
+                Config.Warps.remove(targetN);
+                Config.getWarpFile(targetN).delete();
+                Config.refreshWarpFiles();
+                target.sendMessage(Main.Prefix + ChatColor.YELLOW + "你删除了一个地标 " + targetN);
                 break;
             case UPDATE:
                 Config.Warps.replace(wme.getWarpName(),wme.getNewLocation());
@@ -104,9 +126,14 @@ public class Listeners implements Listener {
                 } catch (IOException | InvalidConfigurationException e) {
                     e.printStackTrace();
                 }
-                target.sendMessage(Main.Prefix + ChatColor.YELLOW + "你更新了" + wme.getWarpName() + "地标的位置" + " 新位置: " + wme.getNewLocation());
+                target.sendMessage(Main.Prefix + ChatColor.YELLOW + "你更新了" + targetN);
                 break;
         }
+    }
+
+    public void teleportTo(Player player,Location target){
+        player.teleport(target);
+        player.sendMessage(Main.Prefix + ChatColor.YELLOW + "你已被传送.");
     }
 
 }
